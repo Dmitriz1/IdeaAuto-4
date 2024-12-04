a9a8d6f7f00d057558a2a3bc7f058637c560a9f3
Dmitriz1 <stud0000264980@study.utmn.ru>
2024-12-04 11:27:43 +0500

    task complited

--- src/test/java/ru/netology/CardOrderTest.java
+++ src/test/java/ru/netology/CardOrderTest.java
@@ -4,25 +4,39 @@ import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.By;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardOrderTest {
     @Test
     public void testOrderForm() {
-        // Открытие страницы приложения
         open("http://localhost:9999");
 
-        $("[data-test-id='city']").setValue("Тюмень");
+        String city = "Тюмень";
+        $(By.cssSelector("[data-test-id='city'] input")).setValue(city);
+        
+        LocalDate currentDate = LocalDate.now();
+        LocalDate deliveryDate = currentDate.plusDays(3);
+        String formattedDate = deliveryDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $(By.cssSelector("[data-test-id='date'] input")).setValue(formattedDate);
 
-        String date = "31.12.2024";
-        $("input[name='date']").setValue(date);
+        String name = "Иванов Иван";
+        $(By.cssSelector("[data-test-id='name'] input")).setValue(name);
 
-        $("[data-test-id='name']").setValue("Иванов Иван");
-        $("[data-test-id='phone']").setValue("+79129262754");
-        $("[data-test-id='agreement']").click();
+        String phone = "+79129262754";
+        $(By.cssSelector("[data-test-id='phone'] input")).setValue(phone);
+
+        $(By.cssSelector("[data-test-id='agreement']")).click();
+
         $(By.className("button")).click();
 
-        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + date));
+        $(By.cssSelector("[data-test-id='notification']"))
+                .shouldHave(text("Встреча успешно забронирована на " + formattedDate))
+                .shouldBe(visible, Duration.ofSeconds(15));
     }
 }
